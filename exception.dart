abf0ffe0274f4e39052e26bae28d53917f0b083f
abyanzz <jilclix7@gmail.com>
2025-07-25 23:53:38 +0700

    Create exception.dart

---
+++ exception.dart
@@ -0,0 +1,19 @@
+// try catch
+
+void main(){
+  try{
+    var a = 7;
+    var b = 0;
+    print(a ~/ b);
+  } on UnsupportedError {
+    print('Can not divide by zero.');
+  }
+  catch (e, s) {
+    print('Exception happened: $e');
+    print('Stack trace: $s');
+  }
+  finally {
+    print('This line still executed');
+  }
+}
+
