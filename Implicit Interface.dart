2e3f1cdadf74f2c993d963c25428393987a96048
abyanzz <jilclix7@gmail.com>
2025-08-05 13:58:31 +0700

    Create Implicit Interface.dart
    Birdy is flying
    Birdy is eating.

---
+++ Implicit Interface.dart
@@ -0,0 +1,38 @@
+class Animal {
+  String name = '';
+  int age = 0;
+  double weight = 0;
+
+  Animal(this.name, this.age, this.weight);
+
+  void eat() {
+    print('$name is eating.');
+    weight = weight + 0.2;
+  }
+
+  void sleep() {
+    print('$name is sleeping.');
+  }
+}
+class Flyable {
+  void fly() {}
+}
+
+class Bird extends Animal implements Flyable {
+  String featherColor;
+
+  Bird(String name, int age, double weight, this.featherColor)
+      : super(name, age, weight);
+
+  @override
+  void fly() {
+    print('$name is flying');
+  }
+}
+
+void main() {
+  var dicodingBird = Bird('Birdy', 2, 0.8, 'Gray');
+
+  dicodingBird.fly();
+  dicodingBird.eat();
+}
