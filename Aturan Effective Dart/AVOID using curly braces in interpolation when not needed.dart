f2e997fcdd4dd1e157aec7bb190ec54f3d7fce11
abyanzz <jilclix7@gmail.com>
2025-08-07 20:15:27 +0700

    Create AVOID using curly braces in interpolation when not needed.dart

---
+++ Aturan Effective Dart/AVOID using curly braces in interpolation when not needed.dart
@@ -0,0 +1,5 @@
+print('Hi, ${name}, You are ${thisYear - birthYear} years old.');
+
+//Namun jika Anda menginterpolasi identifier sederhana, maka curly braces ({}) tidak perlu ditulis.
+
+print('Hi, $name, You are ${thisYear - birthYear} years old.');
