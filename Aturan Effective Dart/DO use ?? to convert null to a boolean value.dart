8d0b43824985f0553f27b6eb48f45c3a050d8acf
abyanzz <jilclix7@gmail.com>
2025-08-07 20:14:50 +0700

    Update DO use ?? to convert null to a boolean value.dart

--- Aturan Effective Dart/DO use ?? to convert null to a boolean value.dart
+++ Aturan Effective Dart/DO use ?? to convert null to a boolean value.dart
@@ -1,3 +1,8 @@
 if(stock?.isEnough) {
   print('Making you a cup of coffee...');
 }
+
+//* Kode di atas akan menghasilkan exception ketika stock-nya null. Untuk mengatasinya kita perlu memberikan nilai default ketika nilai awalnya null. 
+Sehingga kodenya akan menjadi seperti berikut: *//
+
+stock?.isEnough ?? false;
