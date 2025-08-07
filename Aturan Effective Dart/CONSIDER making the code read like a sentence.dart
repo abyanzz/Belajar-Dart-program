b3e8dfcd4801085276504e30f027742b23cc3086
abyanzz <jilclix7@gmail.com>
2025-08-07 20:16:19 +0700

    Create CONSIDER making the code read like a sentence.dart

---
+++ Aturan Effective Dart/CONSIDER making the code read like a sentence.dart
@@ -0,0 +1,9 @@
+// “If store is open ...”
+If (store.isOpen)
+ 
+// “hey garfield, eat!”
+garfield.eat();
+ 
+// Bad example
+// Ambigu apakah memerintahkan toko untuk tutup atau mendapatkan status dari toko
+If (store.close)
